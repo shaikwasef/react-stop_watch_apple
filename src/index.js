b9b80845cce3b86aacf27a834bbd1b9ae3c3931a
shaikwasef <wasefshaik@gmail.com>
2021-02-19 20:31:55 +0530

    3 files modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import "./style.css";
 
-// class StopWatch extends React.Component {
+class StopWatch extends React.Component {
+  render(){
+    return (
+    <body> 
+      <div id="timer" >00:00:00</div>
+      <div class="row_buttons">
+        <div class="buttons lap">Lap</div>
+        <div class="buttons start">Start</div>
+        <div class="buttons reset reset_hidden">Reset</div>
+      </div>
+      <div class="scroll_lap"></div>
+    </body>
+    );
+  }
+}
 
-// }
-
-// ReactDOM.render(<StopWatch /> , document.getElementById('root'));
+ReactDOM.render(<StopWatch /> , document.getElementById('root'));
