795dbc526cb1e9a9cff9e9640920b6597d656e48
shaikwasef <wasefshaik@gmail.com>
2021-02-19 20:34:23 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
+
+
 class StopWatch extends React.Component {
   render(){
     return (
     <body> 
       <div id="timer" >00:00:00</div>
-      <div class="row_buttons">
-        <div class="buttons lap">Lap</div>
-        <div class="buttons start">Start</div>
-        <div class="buttons reset reset_hidden">Reset</div>
+      <div className ="row_buttons">
+        <div className ="buttons lap">Lap</div>
+        <div className ="buttons start">Start</div>
+        <div className ="buttons reset reset_hidden">Reset</div>
       </div>
       <div class="scroll_lap"></div>
     </body>
