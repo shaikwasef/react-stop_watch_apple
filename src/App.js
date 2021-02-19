6b8bb6409bdc6af67fab7bce1a90c334e87b8aa4
shaikwasef <wasefshaik@gmail.com>
2021-02-19 21:11:44 +0530

    Using App.js now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+
+class ScrollLap extends React.Component {
+  render(){
+    return (
+      <div class="scroll_lap"></div>
+    );
+  }
 }
+class StartButton extends React.Component {
+  render() {
+    return (
+    <div className ="buttons start">Start</div>
+    );
+  }
+}
+
+class ResetButton extends React.Component{
+  render() {
+    return (
+      <div className ="buttons reset reset_hidden">Reset</div>
+    );
+  }
+}
+
+class LapButton extends React.Component{
+  render(){
+    return (
+      <div className ="buttons lap">Lap </div>
+    );
+  }
+}
+class Timer extends React.Component{
+  render(){
+    const time = this.props.time ;
+    return (
+      <div id="timer" >00:00:00</div>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.tick = this.tick.bind(this);
+    this.state = {seconds : 0};
+  }
+
+  thisComponentWillMount(){
+    this.timerId = setInterval(this.tick,1000);
+  }
+  
+  tick(){
+    this.setState({seconds : seconds++});
+  }
+
+  render(){
+    return (
+    <body> 
+      <Timer time = {this.state.seconds}/>
+      <div className ="row_buttons">
+        <LapButton />
+        <StartButton/>
+        <ResetButton/>
+      </div>
+      <ScrollLap />
+    </body>
+    );
+  }
+}
+
+export default App;
