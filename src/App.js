32bf993c87d12deb185cbb65432ea6d2ed83d87c
shaikwasef <wasefshaik@gmail.com>
2021-02-22 15:51:56 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import LapButton from "./LapButton.js";
 import Timer from "./Timer.js";
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {
       seconds: 0,
@@ -20,10 +20,12 @@ class App extends React.Component {
     this.tick = this.tick.bind(this);
     this.handleStartClick = this.handleStartClick.bind(this);
     this.handleResetClick = this.handleResetClick.bind(this);
+    this.getMinutes  = this.getMinutes.bind(this);
     this.lapTime = this.lapTime.bind(this);
   }
 
-  handleStartClick() {
+
+  handleStartClick(){
     const start_button = !this.state.start;
     this.setState({ start: start_button });
     if (start_button == 0) {
@@ -38,11 +40,9 @@ class App extends React.Component {
     this.setState({ seconds: 0, minutes: 0, hours: 0, start: true });
   }
 
-  tick() {
-    var seconds = this.state.seconds;
-    var minutes = this.state.minutes;
-    var hours = this.state.hours;
-    seconds = seconds + 1;
+  tick(){
+    seconds = this.state.seconds ;
+    seconds = seconds + 1 ;
     if (seconds > 59) {
       minutes = Math.floor(seconds / 60);
       seconds = 0;
@@ -51,7 +51,7 @@ class App extends React.Component {
       hours = Math.floor(minutes / 60);
       minutes = 0;
     }
-    this.setState({ seconds: seconds, minutes: minutes, hours: hours });
+    this.setState({seconds : seconds , minutes: minutes, hours: hours});
   }
 
   render() {
