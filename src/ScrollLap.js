2bf1a5f895362c381a6f8193df9129f9a227f787
shaikwasef <wasefshaik@gmail.com>
2021-02-22 16:59:17 +0530

    2 files modified

--- src/ScrollLap.js
+++ src/ScrollLap.js
@@ -6,9 +6,7 @@ class ScrollLap extends React.Component {
   render(){
     return (
       <div className="scroll_lap">
-      {this.props.lap_content.map((lap, index) => (
-        <div className = "">Hello, {person.name} from {person.country}!</p>
-      ))}
+      
       </div>
     );
   }
